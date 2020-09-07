e07468536437d98c17d1427c257ec6c4c2c06bb9
Lepiku <git.lepiku@gmail.com>
2020-09-07 20:10:22 +0700

    use xidlehook for autolock and install dispad

--- bin/run-xidlehook.sh
+++ bin/run-xidlehook.sh
@@ -4,8 +4,11 @@ xidlehook \
   `# Don't lock when there's a fullscreen application` \
   --not-when-fullscreen \
   `# Don't lock when there's audio playing` \
-  #--not-when-audio \
+  --not-when-audio \
   `# sleep after 15 minutes` \
   --timer 900 \
-    'systemctl suspend' \
+    'xset dpms force off' \
+    'xset dpms force on' \
+  --timer 10 \
+    'systemctl suspend & lock' \
     ''
