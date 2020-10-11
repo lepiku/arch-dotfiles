2c4e3995005de209e9fab3aabd483142c6786365
Lepiku <git.lepiku@gmail.com>
2020-10-12 06:25:35 +0700

    shutdown on 5% battery

--- bin/BatNotif.sh
+++ bin/BatNotif.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Low battery warning script
 
+off=5
 low=15
 high=65
 
@@ -11,7 +12,9 @@ full_level=`cat /sys/class/power_supply/BAT0/charge_full_design`
 current_level=`expr "$level" \* 100 / "$full_level"`
 
 echo "Battery: $current_level%"
-if [ ! -n "$charging" ] && [ "$current_level" -lt "$low" ]; then
+if [ ! -n "$charging" ] && [ "$current_level" -lt "$off" ]; then
+    poweroff
+elif [ ! -n "$charging" ] && [ "$current_level" -lt "$low" ]; then
     i3-nagbar -f "pango:SourceCodePro SemiBold 10" -m "LOW BATTERY: Battery < $low%"
 elif [ -n "$charging" ] && [ "$current_level" -ge "$high" ];then
     i3-nagbar -f "pango:SourceCodePro SemiBold 10" -m "FULL BATTERY: Battery > $high%" -t "warning"
