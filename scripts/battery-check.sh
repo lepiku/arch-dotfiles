43fbd5e81843b4454f9be93eb82aff383680c93c
Lepiku <git.lepiku@gmail.com>
2022-11-12 08:16:44 +0700

    config: store script variables in .config/script.conf

--- scripts/battery-check.sh
+++ scripts/battery-check.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 # Low battery warning script
 
-#off=5
-low=15
-high=42
+source ~/.config/script.conf
+: '
+$battery_low
+$battery_high
+$battery_notify_high
+'
 
 charging=`acpi -b | grep "Charging"`
 level=`cat /sys/class/power_supply/BAT0/charge_now`
 full_level=`cat /sys/class/power_supply/BAT0/charge_full_design`
 current_level=`expr "$level" \* 100 / "$full_level"`
 
-if [ ! -n "$charging" ] && [ "$current_level" -lt "$low" ]; then
+if [ ! -n "$charging" ] && [ "$current_level" -lt "$battery_low" ]; then
     notify-send -u critical "LOW BATTERY ($current_level%)"
-#elif [ -n "$charging" ] && [ "$current_level" -ge "$high" ];then
-#    notify-send -u normal "FULL BATTERY ($current_level%)"
+elif [ $battery_notify_high = true ] \
+        && [ -n "$charging" ] \
+        && [ "$current_level" -ge "$battery_high" ]; then
+    notify-send -u normal "FULL BATTERY ($current_level%)"
 fi
