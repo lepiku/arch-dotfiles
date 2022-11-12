6951e2c94dca8f8d4f394016a46428141846d7d2
Lepiku <git.lepiku@gmail.com>
2022-11-12 22:31:57 +0700

    battery: add app_name to notification

--- scripts/battery-check.sh
+++ scripts/battery-check.sh
@@ -8,15 +8,19 @@ $battery_high
 $battery_notify_high
 '
 
+app_name="battery-check.sh"
+
 charging=`acpi -b | grep "Charging"`
 level=`cat /sys/class/power_supply/BAT0/charge_now`
 full_level=`cat /sys/class/power_supply/BAT0/charge_full_design`
 current_level=`expr "$level" \* 100 / "$full_level"`
 
 if [ ! -n "$charging" ] && [ "$current_level" -lt "$battery_low" ]; then
-    notify-send -u critical "LOW BATTERY ($current_level%)"
+    notify-send -u critical -a $app_name "LOW BATTERY ($current_level%)"
 elif [ $battery_notify_high = true ] \
         && [ -n "$charging" ] \
         && [ "$current_level" -ge "$battery_high" ]; then
-    notify-send -u normal "FULL BATTERY ($current_level%)"
+    notify-send -u low -a $app_name "Full Battery ($current_level%)"
+else
+    echo "Nothing to do."
 fi
