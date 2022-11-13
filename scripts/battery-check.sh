40d846434f84c8df7d62ed3c499efb74006a592f
Lepiku <git.lepiku@gmail.com>
2022-11-13 10:22:29 +0700

    dunst: show icon

--- scripts/battery-check.sh
+++ scripts/battery-check.sh
@@ -16,11 +16,13 @@ full_level=`cat /sys/class/power_supply/BAT0/charge_full_design`
 current_level=`expr "$level" \* 100 / "$full_level"`
 
 if [ ! -n "$charging" ] && [ "$current_level" -lt "$battery_low" ]; then
-    notify-send -u critical -a $app_name "LOW BATTERY ($current_level%)"
+    notify-send -u critical -a $app_name -i xfce4-battery-critical \
+        "LOW BATTERY ($current_level%)" "Connect charger!"
 elif [ $battery_notify_high = true ] \
         && [ -n "$charging" ] \
         && [ "$current_level" -ge "$battery_high" ]; then
-    notify-send -u low -a $app_name "Full Battery ($current_level%)"
+    notify-send -u normal -a $app_name -i battery-full-charged \
+        "Full Battery ($current_level%)" "Disconnect charger!"
 else
     echo "Nothing to do."
 fi
