d25163f94bbd681807a7be5ad7217f9857a0aad1
Lepiku <git.lepiku@gmail.com>
2022-11-25 16:35:18 +0700

    battery: notify low or full battery only once

--- scripts/battery-check.sh
+++ scripts/battery-check.sh
@@ -7,6 +7,11 @@ $battery_low
 $battery_high
 $battery_notify_high
 '
+source ~/.config/script-state.sh
+: '
+$battery_notified_low_battery
+$battery_notified_high_battery
+'
 
 app_name="battery-check.sh"
 
@@ -15,14 +20,28 @@ level=`cat /sys/class/power_supply/BAT0/charge_now`
 full_level=`cat /sys/class/power_supply/BAT0/charge_full_design`
 current_level=`expr "$level" \* 100 / "$full_level"`
 
-if [ ! -n "$charging" ] && [ "$current_level" -lt "$battery_low" ]; then
-    notify-send -u critical -a $app_name -i xfce4-battery-critical \
-        "LOW BATTERY ($current_level%)" "Connect charger!"
+if [ ! -n "$charging" ]  && [ "$current_level" -lt "$battery_low" ]; then
+    echo "Low battery."
+    if [ $battery_notified_low_battery = false ]; then
+        echo "Notified."
+        set-script-state "battery_notified_low_battery" true
+        notify-send -u critical -a $app_name -i xfce4-battery-critical -t 0 \
+            "LOW BATTERY ($current_level%)" "Connect charger\!"
+    fi
+
 elif [ $battery_notify_high = true ] \
         && [ -n "$charging" ] \
         && [ "$current_level" -ge "$battery_high" ]; then
-    notify-send -u normal -a $app_name -i battery-full-charged \
-        "Full Battery ($current_level%)" "Disconnect charger!"
+    echo "Full battery."
+    if [ $battery_notified_high_battery = false ]; then
+        echo "Notified."
+        set-script-state "battery_notified_high_battery" true
+        notify-send -u normal -a $app_name -i battery-full-charged -t 0 \
+            "Full Battery ($current_level%)" "Disconnect charger\!"
+    fi
+
 else
     echo "Nothing to do."
+    set-script-state "battery_notified_low_battery" false
+    set-script-state "battery_notified_high_battery" false
 fi
