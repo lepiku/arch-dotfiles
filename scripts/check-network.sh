c1e81b9bcf8fb867ebc0355555d52b7513b26dc4
Muhammad Oktoluqman Fakhrianto <dimas@okuto.id>
2026-02-28 06:37:50 +0700

    feat(script): auto reconnect to wifi

--- scripts/check-network.sh
+++ scripts/check-network.sh
@@ -6,5 +6,10 @@ if ! ping -c 5 10.13.13.1
 then
     # restart internet
     echo "Restarting internet..."
-    nmcli connection up 'Auto Ethernet'
+
+    #nmcli connection down 'Auto Ethernet'
+    #sleep 5
+    #nmcli connection up 'Auto Ethernet'
+
+    nmcli connection up 'O419_5G'
 fi
